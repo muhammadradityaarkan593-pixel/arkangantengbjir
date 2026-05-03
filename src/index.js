99a9864b5d4d5ea2e9bc2b32373078b5377d1cef
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-03 19:25:43 +0700

    Refactor response for /docs and update home message

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
+import docsHtml from './docs.html';
+
 export default {
   async fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname === "/docs") {
-      return new Response(`
-        <html>
-          <body style="background:black;color:white">
-            <h1>DOCS BERHASIL</h1>
-          </body>
-        </html>
-      `, {
+      return new Response(docsHtml, {
         headers: {
-          "content-type": "text/html;charset=UTF-8"
+          "content-type": "text/html; charset=UTF-8"
         }
       });
     }
 
-    return new Response("HOME");
+    return new Response("Home Page");
   }
 }
