43da6780f28e3d8183f0d0a244ee36c1bfeff642
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-03 19:17:24 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import docsHtml from './docs.html';
-
 export default {
   async fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname === "/docs") {
-      return new Response(docsHtml, {
+      return new Response(`
+        <html>
+          <body style="background:black;color:white">
+            <h1>DOCS BERHASIL</h1>
+          </body>
+        </html>
+      `, {
         headers: {
-          "content-type": "text/html; charset=UTF-8"
+          "content-type": "text/html;charset=UTF-8"
         }
       });
     }
 
-    return new Response("Home Page");
+    return new Response("HOME");
   }
 }
