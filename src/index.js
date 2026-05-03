4b466f3675b179a8d96fd715b9ae097a7f7de160
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-03 19:10:35 +0700

    Add fetch handler for '/docs' route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import docsHtml from '../docs.html';
 
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/docs") {
+      return new Response(docsHtml, {
+        headers: {
+          "content-type": "text/html; charset=UTF-8"
+        }
+      });
+    }
+
+    return new Response("Home Page");
+  }
+}
