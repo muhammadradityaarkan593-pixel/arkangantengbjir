27f5ab1bca0868d669754e273604d8b3deb234b9
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-03 19:31:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import docsHtml from './docs.html';
+import home from "../index.html";
+import docs from "./docs.html";
 
 export default {
   async fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname === "/docs") {
-      return new Response(docsHtml, {
-        headers: {
-          "content-type": "text/html; charset=UTF-8"
-        }
+      return new Response(docs, {
+        headers: { "content-type": "text/html;charset=UTF-8" }
       });
     }
 
-    return new Response("Home Page");
+    if (url.pathname === "/") {
+      return new Response(home, {
+        headers: { "content-type": "text/html;charset=UTF-8" }
+      });
+    }
+
+    return new Response("404 Not Found", { status: 404 });
   }
-}
+};
