46ade24094067706789762efc1625f06d78700ae
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-04 15:15:36 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import home from "../index.html";
 import docs from "./docs.html";
 import { handleTikTok } from "./tiktok.js";
+import { handleInstagram } from "./instagram.js";
 
 export default {
   async fetch(request, env, ctx) {
@@ -29,23 +30,28 @@ export default {
       return handleTikTok(request);
     }
 
+    // API Instagram
+    if (url.pathname.startsWith("/api/instagram")) {
+      return handleInstagram(request);
+    }
+
     // File Proxy
     if (url.pathname.startsWith("/file/")) {
-  const src = url.searchParams.get("src");
-  if (!src) {
-    return new Response("No source", { status: 400 });
-  }
+      const src = url.searchParams.get("src");
+      if (!src) {
+        return new Response("No source", { status: 400 });
+      }
 
-  const res = await fetch(decodeURIComponent(src));
-  const contentType = res.headers.get("content-type") || "application/octet-stream";
+      const res = await fetch(decodeURIComponent(src));
+      const contentType = res.headers.get("content-type") || "application/octet-stream";
 
-  return new Response(res.body, {
-    headers: {
-      "content-type": contentType,
-      "Content-Disposition": "inline",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
+      return new Response(res.body, {
+        headers: {
+          "content-type": contentType,
+          "Content-Disposition": "inline",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
     }
 
     return new Response("404 Not Found", { status: 404 });
