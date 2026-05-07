f5e02bef23d275c91fb52100005c49bda386f2c8
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-05-07 18:45:29 +0700

    Add fetch function to cldf.js for HTML response

--- cldf.js
+++ cldf.js
@@ -1,1 +1,8 @@
-
+export default {
+  async fetch(request) {
+    const html = await import('./haha.html', { assert: { type: 'text' } });
+    return new Response(html.default, {
+      headers: { 'Content-Type': 'text/html' },
+    });
+  }
+};
