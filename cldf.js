6bd0043feb0be2f60c27780d5b946f6386a19abc
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-07-29 17:42:07 +0700

    Delete cldf.js

--- cldf.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request) {
-    const html = await import('./haha.html', { assert: { type: 'text' } });
-    return new Response(html.default, {
-      headers: { 'Content-Type': 'text/html' },
-    });
-  }
-};
